0a9b800f34b0d6ad54db75f9618b54f093c081eb
Unknown <stheadman1@hotmail.com>
2018-05-21 20:02:04 -0600

    UHHH Laser Sword

---
+++ 2D Top Down/Assets/2D Top DOWN VN/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float speed;
+    public Transform player;
+    public Rigidbody2D ERB;
+
+    void FixedUpdate()
+    {
+        float z = Mathf.Atan2((player.transform.position.y - transform.position.y), (player.transform.position.x - transform.position.x)) * Mathf.Rad2Deg - 90;
+
+        transform.eulerAngles = new Vector3(0, 0, z);
+
+        ERB.AddForce(gameObject.transform.up * speed);
+    }
+}
---
+++ 2D Top Down/Assets/2D Top DOWN VN/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public Rigidbody2D RB;
+
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        RB.angularVelocity = 0;
+
+        float input = Input.GetAxis("Vertical");
+        RB.AddForce(gameObject.transform.up * speed * input);
+    }
+}
