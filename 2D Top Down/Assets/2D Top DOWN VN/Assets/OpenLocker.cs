aa88a6ef4041d3d70d9f9fd2aa5f3e8e73eff303
Unknown <stheadman1@hotmail.com>
2018-07-06 19:18:20 -0600

    Working on Locker Animation

---
+++ 2D Top Down/Assets/2D Top DOWN VN/Assets/OpenLocker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLocker : MonoBehaviour {
+
+    float duration = 0.5f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Rotate(duration));
+        } else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(RotateBack(duration));
+        }
+    }
+
+    IEnumerator Rotate(float duration)
+    {
+        float startRotation = transform.eulerAngles.y;
+        float endRotation = startRotation - 45.0f;
+        float t = 0.0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float yRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+            yield return null;
+        }
+    }
+
+    IEnumerator RotateBack(float duration)
+    {
+        float startRotation = transform.eulerAngles.y;
+        float endRotation = startRotation  + 45.0f;
+        float t = 0.0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float yRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, yRotation, transform.eulerAngles.z);
+            yield return null;
+        }
+    }
+}
